df6feed4db1eac07ca2b818c2fa1c94e1bc59492
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-15 19:42:06 -0500

    add database cleaner config to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,19 @@ require 'database_cleaner'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
