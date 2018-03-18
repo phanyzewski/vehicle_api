79250577414c36b4a18f6d1cdefbf889b997a73a
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-18 18:19:04 -0500

    rename column to avoid name collisions

--- spec/requests/vehicle_spec.rb
+++ spec/requests/vehicle_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Vehicles API', type: :request do
 
   let(:vehicle_id) { vehicles.first.id }
   let(:vin) { vehicles.first.vin }
+  let(:option_id) { vehicles.first.options.first.id }
 
   describe 'GET /vehicles' do
     before { get '/vehicles' }
@@ -86,6 +87,34 @@ RSpec.describe 'Vehicles API', type: :request do
 
     it 'returns status code 204' do
       expect(response).to have_http_status(:no_content)
+    end
+  end
+
+  describe 'update vhicle options' do
+    before { put "/vehicles/#{vehicle_id}/options/#{option_id}", params: valid_attributes }
+    let(:valid_attributes) { { option_id: option_id, option_included: true } }
+
+    context 'when vehicle option exists' do
+      it 'returns status code 204' do
+        expect(response).to have_http_status(:no_content)
+      end
+
+      it 'updates the item' do
+        vehicle_options = Vehicle.find_by(vehicle_id: vehicle_id).options
+        updated_option = vehicle_options.select { |o| o.id == option_id }.first
+        expect(updated_option.option_included).to be true
+      end
+    end
+    context 'when the option does not exist' do
+      let(:option_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Option/)
+      end
     end
   end
 end
