c6b429d5154d1cad2d2ad0442071cd19e1fca2df
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-18 18:13:38 -0500

    add joins table and model for vehicle -- options

---
+++ db/migrate/20180318154140_create_join_table_options_vehicles.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableOptionsVehicles < ActiveRecord::Migration[5.1]
+  def change
+    create_join_table  :options, :vehicles do |t|
+      t.index [:vehicle_id, :option_id]
+      t.index [:option_id, :vehicle_id]
+    end
+  end
+end
