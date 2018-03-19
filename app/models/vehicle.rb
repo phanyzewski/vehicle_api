1028f0546806b8f10bf74cde4bfb0e06b7ba12d1
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-18 20:41:59 -0500

    add db seed, remove direct association between vehicle and make

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,5 @@
 class Vehicle < ApplicationRecord
   belongs_to :vehicle_model
-  belongs_to :vehicle_make
 
   has_many :options_vehicle
   has_many :options, through: :options_vehicle
--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :vehicle do
-    association :vehicle_make, strategy: :build
     association :vehicle_model, strategy: :build
 
     vin { Faker::Vehicle.vin }
