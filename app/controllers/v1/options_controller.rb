f6b2a340981a9ec52c2ffcbb01e4abd56413e637
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-18 20:46:41 -0500

    rename controllers as v1

---
+++ app/controllers/v1/options_controller.rb
@@ -0,0 +1,39 @@
+module V1
+  class OptionsController < ApplicationController
+    before_action :set_option, only: [:show, :update, :destroy]
+
+    def index
+      @options = Option.all
+      json_response(@options)
+    end
+
+    def create
+      @option = Option.create!(option_params)
+      json_response(@option, :created)
+    end
+
+    def show
+      json_response(@option)
+    end
+
+    def update
+      @option.update(option_params)
+      head :no_content
+    end
+
+    def destroy
+      @option.destroy
+      head :no_content
+    end
+
+    private
+
+    def option_params
+      params.permit(:name, :included)
+    end
+
+    def set_option
+      @option = Option.find(params[:id])
+    end
+  end
+end
