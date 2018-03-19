f9964bf1285bc0329daef087902aa764f02a4478
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-18 20:46:41 -0500

    rename controllers as v1

---
+++ app/controllers/v1/vehicle_models_controller.rb
@@ -0,0 +1,39 @@
+module V1
+  class VehicleModelsController < ApplicationController
+    before_action :set_vehicle_model, only: [:show, :update, :destroy]
+
+    def index
+      @vehicle_models = VehicleModel.all
+      json_response(@vehicle_models)
+    end
+
+    def create
+      @vehicle_model = VehicleModel.create!(vehicle_model_params)
+      json_response(@vehicle_model, :created)
+    end
+
+    def show
+      json_response(@vehicle_model)
+    end
+
+    def update
+      @vehicle_model.update(vehicle_model_params)
+      head :no_content
+    end
+
+    def destroy
+      @vehicle_model.destroy
+      head :no_content
+    end
+
+    private
+
+    def vehicle_model_params
+      params.permit(:name, :vehicle_make_id)
+    end
+
+    def set_vehicle_model
+      @vehicle_model = VehicleModel.find(params[:id])
+    end
+  end
+end
