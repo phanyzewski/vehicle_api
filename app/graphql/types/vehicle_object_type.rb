bb216c1002361f7b57f7696052baf4f21fa7fac6
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-20 20:19:58 -0500

    updated readme

--- app/graphql/types/vehicle_object_type.rb
+++ app/graphql/types/vehicle_object_type.rb
@@ -4,7 +4,7 @@ Types::VehicleObjectType = GraphQL::ObjectType.define do
   name 'Vehicle'
   field :vin, types.ID
   field :mileage, types.Int
-  field :model, Types::VehicleModelObjectType
-  field :make, Types::VehicleMakeObjectType
+  field :vehicle_model, Types::VehicleModelObjectType
+  field :vehicle_make, Types::VehicleMakeObjectType
   field :options, types[Types::OptionObjectType]
 end
