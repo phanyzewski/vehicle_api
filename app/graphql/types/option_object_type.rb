432977d210e0ba211384b6fc1b50f4131ad86b21
Paul Hanyzewski <phanyzewski@enova.com>
2018-03-20 20:25:18 -0500

    type descriptions

--- app/graphql/types/option_object_type.rb
+++ app/graphql/types/option_object_type.rb
@@ -2,8 +2,11 @@
 
 Types::OptionObjectType = GraphQL::ObjectType.define do
   name 'Option'
+  description 'An option, sometimes it is a standard option included with a particular make/model,
+    sometimes it is extra and specific only to the vehicle itself'
+
   field :name, types.String
   field :option_included, types.Boolean
-  field :models, types[Types::VehicleModelObjectType]
+  field :vehicle_models, types[Types::VehicleModelObjectType]
   field :vehicles, types[Types::VehicleObjectType]
 end
--- app/graphql/types/vehicle_object_type.rb
+++ app/graphql/types/vehicle_object_type.rb
@@ -2,6 +2,8 @@
 
 Types::VehicleObjectType = GraphQL::ObjectType.define do
   name 'Vehicle'
+  description 'A Vehicle, commonly driven on a road'
+
   field :vin, types.ID
   field :mileage, types.Int
   field :vehicle_model, Types::VehicleModelObjectType
